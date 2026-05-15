7532d7e7a5da330ebf36ba2dbf0c1f1851dbb145
MiLy3009 <emilycruz3009@gmail.com>
2026-05-14 20:01:00 -0500

    kkk

--- Assets/codigos/tranciones/BotonPanel.cs
+++ Assets/codigos/tranciones/BotonPanel.cs
@@ -4,15 +4,26 @@ public class BotonPanel : MonoBehaviour
 {
     public GameObject panelActual;
     public GameObject panelNuevo;
+    public bool abrirRompecabezas = false;
     private PanelTransitionManager manager;
+    private PuzzleManager puzzleManager;
 
     void Start()
     {
         manager = FindAnyObjectByType<PanelTransitionManager>();
+        puzzleManager = FindAnyObjectByType<PuzzleManager>();
     }
 
     public void AlAplastar()
     {
-        manager.CambiarPanel(panelActual, panelNuevo);
+        if (abrirRompecabezas && puzzleManager != null)
+        {
+            panelActual.SetActive(false);
+            puzzleManager.AbrirRompecabezas();
+        }
+        else
+        {
+            manager.CambiarPanel(panelActual, panelNuevo);
+        }
     }
 }
