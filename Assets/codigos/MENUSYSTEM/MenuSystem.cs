8bf6e11233987225397b297b7a982323a88878c9
MiLy3009 <emilycruz3009@gmail.com>
2026-06-10 18:10:26 -0500

    b

--- Assets/codigos/MENUSYSTEM/MenuSystem.cs
+++ Assets/codigos/MENUSYSTEM/MenuSystem.cs
@@ -3,10 +3,7 @@ using UnityEngine.SceneManagement;
 
 public class MenuSystem : MonoBehaviour
 {
-    public void IniciarSimulacion()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-    }
+   
 
     public void Salir()
     {
