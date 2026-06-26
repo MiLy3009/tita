ee1ed96de4f282aea8b4ca4abd3969d3c23305f6
MiLy3009 <emilycruz3009@gmail.com>
2026-06-26 13:07:03 -0500

    cambios

---
+++ Assets/codigos/tranciones/TextoEscribiendose.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class TextoEscribiendose : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
