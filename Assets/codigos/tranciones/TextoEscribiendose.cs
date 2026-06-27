9123130ebc8fe5f5b0cab247ac8b79e7588a3f76
MiLy3009 <emilycruz3009@gmail.com>
2026-06-26 19:03:46 -0500

    cambios

--- Assets/codigos/tranciones/TextoEscribiendose.cs
+++ Assets/codigos/tranciones/TextoEscribiendose.cs
@@ -1,16 +1,58 @@
+using System.Collections;
 using UnityEngine;
+using TMPro;
 
 public class TextoEscribiendose : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [Header("Configuración")]
+    public TextMeshProUGUI textoUI;
+    [TextArea(3, 10)]
+    public string textoCompleto = "Escribe aquí tu texto...";
+    public float velocidad = 0.05f; // segundos entre cada letra
+    public bool iniciarAutomatico = true;
+
+    private Coroutine coroutinaActual;
+
     void Start()
     {
-        
+        if (iniciarAutomatico)
+            MostrarTexto(textoCompleto);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void MostrarTexto(string texto)
     {
-        
+        if (coroutinaActual != null)
+            StopCoroutine(coroutinaActual);
+
+        textoCompleto = texto;
+        coroutinaActual = StartCoroutine(EscribirTexto());
+    }
+
+    IEnumerator EscribirTexto()
+    {
+        textoUI.text = "";
+
+        foreach (char letra in textoCompleto)
+        {
+            textoUI.text += letra;
+            yield return new WaitForSeconds(velocidad);
+        }
+
+        coroutinaActual = null;
+    }
+
+    public void Saltar()
+    {
+        if (coroutinaActual != null)
+        {
+            StopCoroutine(coroutinaActual);
+            textoUI.text = textoCompleto;
+            coroutinaActual = null;
+        }
+    }
+
+    public bool EstaEscribiendo()
+    {
+        return coroutinaActual != null;
     }
 }
